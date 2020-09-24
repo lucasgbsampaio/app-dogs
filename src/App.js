31421fe0d8725f56f35544fb59c6cb67204a55d3
Lucas <lukas-gabriel2002@hotmail.com>
2020-09-24 20:30:49 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
-export default function App() {
-  return <div>App React</div>;
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import { UserContext } from '../UserContext';
+
+const Header = () => {
+  const { data, userLogout } = React.useContext(UserContext);
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+            <button onClick={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
